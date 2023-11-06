f2a01d469bce845d15058d517194d7500a8f2015
Daniel9822 <dhdany64@gmail.com>
2023-11-06 18:58:22 -0400

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { AuthModule } from './auth/auth.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { secrets } from './utils/envs'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MongooseModule.forRoot(secrets.MONGO_URI), UserModule],
   controllers: [AppController],
   providers: [AppService]
 })
