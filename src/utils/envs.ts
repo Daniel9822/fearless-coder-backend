96b6f3db3dce2d05d73950ba718a1531036ce423
Daniel9822 <dhdany64@gmail.com>
2023-11-06 18:59:10 -0400

    update

--- src/utils/envs.ts
+++ src/utils/envs.ts
@@ -5,5 +5,6 @@ export const secrets = {
   JWT_SECRET: process.env.JWT_SECRET,
   CALLBACK_URL: process.env.CALLBACK_URL,
   GOOGLE_SECRET_ID: process.env.GOOGLE_SECRET_ID,
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  MONGO_URI: process.env.MONGO_URI
 }
