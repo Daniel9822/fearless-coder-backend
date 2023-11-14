219c1dd02290f63f0dd309ee9df4f3c1b66d93b7
Daniel9822 <dhdany64@gmail.com>
2023-11-14 18:51:11 -0400

    style update

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,13 +4,18 @@ import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './auth.controllers'
 import { JwtModule } from '@nestjs/jwt'
 import { secrets } from 'src/utils/envs'
+import { UserModule } from 'src/user/user.module'
 
 @Module({
-  imports: [PassportModule, JwtModule.register({
-    global: true,
-    secret: secrets.JWT_SECRET,
-    signOptions: { expiresIn: '60s' }
-  })],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      global: true,
+      secret: secrets.JWT_SECRET,
+      signOptions: { expiresIn: '60s' }
+    }),
+    UserModule
+  ],
   controllers: [AuthController],
   providers: [AuthServiceGoogle]
 })
