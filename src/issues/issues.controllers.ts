a1e0f06e213211f15e73af1957e4f69a2241000a
Daniel9822 <dhdany64@gmail.com>
2023-11-20 18:49:23 -0400

    update

--- src/issues/issues.controllers.ts
+++ src/issues/issues.controllers.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common'
 import { IssuesService } from './issues.service'
 import { issueDto } from './Dto/issue.dto'
 
@@ -15,5 +15,16 @@ export class IssuesController {
   async createIssue(@Body() issue: issueDto) {
     const data = await this.issueServise.create(issue)
     return data
+  }
+
+  @Put(':id')
+  async updateIssue(@Body() issue: issueDto, @Param('id') id: string) {
+    const data = await this.issueServise.updateItem(issue, id)
+    return data
+  }
+
+  @Delete(':id')
+  async deleteIssue(@Param('id') id: string) {
+    return await this.issueServise.deleteItem(id)
   }
 }
